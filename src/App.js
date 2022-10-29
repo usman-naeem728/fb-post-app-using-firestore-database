85e4921c1705039301f11e9bbce08fd6ce68f73b
Muhammad Usman Naeem <76055167+usman-naeem728@users.noreply.github.com>
2022-10-28 20:55:21 -0700

    firestore database

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { useState, useEffect } from 'react';
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { collection, addDoc, getDocs } from "firebase/firestore";
-import { query, onSnapshot } from "firebase/firestore";
+import {
+  query, onSnapshot, serverTimestamp,
+  orderBy, deleteDoc, doc, updateDoc
+} from "firebase/firestore";
 import { async } from '@firebase/util';
 import moment from 'moment/moment';
-
+import profilepic from "./profileimg.jpg";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDSn9S0e11dBXK2hAfD6o5uo_-lJxeQEiQ",
@@ -31,7 +34,10 @@ function App() {
   const [posttext, setposttext] = useState("")
   const [posttitte, setposttittle] = useState("")
 
-
+  const [editing, setEditing] = useState({
+    editingId: null,
+    editingText: ""
+  })
 
 
   const savePost = async (e) => {
@@ -41,9 +47,8 @@ function App() {
 
     try {
       const docRef = await addDoc(collection(db, "posts"), {
-        tittle: posttitte,
         posttext: posttext,
-        createdon: new Date().getTime(),
+        createdon: serverTimestamp(),
       });
       console.log("Document written with ID: ", docRef.id);
     } catch (e) {
@@ -52,8 +57,30 @@ function App() {
 
   }
 
+  const deletePost = async (postId) => {
+    await deleteDoc(doc(db, "posts", postId));
+  }
+
+  // const updatepost = async (postId, updatetext) => {
+  //   await updateDoc(doc(db, "posts", postId), {
+  //     text: updatetext
+  //   });
+  // }
 
 
+  const updatePost = async (e) => {
+    e.preventDefault();
+
+    await updateDoc(doc(db, "posts", editing.editingId), {
+      posttext: editing.editingText
+    });
+
+    setEditing({
+      editingId: null,
+      editingText: ""
+    })
+
+  }
   useEffect(() => {
 
     const getData = async () => {
@@ -78,11 +105,12 @@ function App() {
     const getrealtimedata = async () => {
 
 
-      const q = query(collection(db, "posts"));
+      const q = query(collection(db, "posts"), orderBy('createdon', 'desc'));
       unsubscribe = onSnapshot(q, (querySnapshot) => {
         const posts = [];
         querySnapshot.forEach((doc) => {
-          posts.push(doc.data());
+
+          posts.push({ id: doc.id, ...doc.data() });
         });
 
         setpost(posts)
@@ -107,7 +135,7 @@ function App() {
 
 
   return (
-    <div >
+    <div className='main'>
       <div>
         <div className="nav1">
           <h2>Facebook Posts</h2>
@@ -115,21 +143,48 @@ function App() {
       </div>
 
 
+      <div className='container1'>
+        <div className='propic'>
+          <img src={profilepic} />
+        </div>
+        <div className='inputpost'>
+          <input placeholder="What's in your mind" data-bs-toggle="modal" data-bs-target="#exampleModal" disabled />
+        </div>
+      </div>
 
 
+
+      {/* <!-- Modal --> */}
+      <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header ">
+              <h4 className="modal-title text-center" >Create Post</h4>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <form className="input1" onSubmit={savePost}>
+                <input  placeholder="What's in your mind" onChange={(e) => {
+                  setposttext(e.target.value)
+                }} />
+                <button className="btn btn-outline-secondary " type="submit">Post</button>
+              </form>
+            </div>
+            {/* <div className="modal-footer">
+              <button type="button" className="btn btn-primary">Save changes</button>
+            </div> */}
+          </div>
+        </div>
+      </div>
       {/* input search bar  */}
-      <div className='container'>
-        <form className="input-group my-5" onSubmit={savePost}>
-          <input type="text" placeholder="Enter your tittle" onChange={(e) => {
-            setposttittle(e.target.value)
-          }} />
-          <br />
-          <textarea type="text" className="form-control" placeholder="What's in your mind" onChange={(e) => {
+      {/* <div className='container1'>
+        <form className="input1 my-5" onSubmit={savePost}>
+          <input type="text" className="form-control" placeholder="What's in your mind" onChange={(e) => {
             setposttext(e.target.value)
           }} />
           <button className="btn btn-outline-secondary" type="submit">Save post</button>
         </form>
-      </div>
+      </div> */}
 
       {/* now start posting news  */}
       <div className='container'>
@@ -146,9 +201,50 @@ function App() {
                 <div className="col-md-8">
                   <div className="card-body">
                     <h1 className="card-title"><b> {eachPost.tittle} </b></h1>
-                    <p className="card-text"><i> {eachPost.posttext} </i></p>
-                    <h6>{moment(eachPost.createdon).format('MMMM Do YYYY, h:mm:ss a')}</h6>
+                    <h3 className="card-text">
+                      <i>
+                        {(eachPost.id === editing.editingId) ?
+                          <form onSubmit={updatePost}>
+                            <input
+                              type="text"
+                              value={editing.editingText}
+                              onChange={(e) => {
+                                setEditing({
+                                  ...editing,
+                                  editingText: e.target.value
+                                })
+                              }}
+                              placeholder="please enter updated value" />
+                            <button type="submit">Update</button>
+                          </form>
+                          :
+                          eachPost?.posttext
+                        }
+                      </i>
+                    </h3>
+                    <h6>{moment((eachPost?.createdon?.seconds) ?
+                      eachPost?.createdon?.seconds * 1000
+                      :
+                      undefined)
+                      .format('MMMM Do YYYY, h:mm a')}</h6>
                   </div>
+                  <button onClick={() => {
+
+                    deletePost(eachPost?.id)
+                  }
+                  } >delete</button>
+
+
+                  {(editing.editingId === eachPost?.id) ? null :
+                    <button onClick={() => {
+
+                      setEditing({
+                        editingId: eachPost?.id,
+                        editingText: eachPost?.posttext
+                      })
+
+                    }} >Edit</button>
+                  }
                 </div>
               </div>
             </div>
